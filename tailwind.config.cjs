524a62a8dffd0371d0fea73766077e0192425a2d
WalterCun <waltercunbustamante@gmail.com>
2025-10-02 12:34:36 -0500

    Implementacion de base de codigo de pagina web fase 1.5 - Implementacion de paleta de colores

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#29333B', // Azul Marino Oscuro
+        'accent': '#BF953D',  // Dorado Antiguo
+        'background': '#ffffff', // Gris Claro/Beige
+      },
+    },
   },
   plugins: [],
 }

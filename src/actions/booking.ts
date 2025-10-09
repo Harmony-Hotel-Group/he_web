f0871dc69310b4f58a4fbb9cf32a120a927102bd
WalterCun <waltercunbustamante@gmail.com>
2025-10-09 09:32:16 -0500

    Implementacion de base de codigo de pagina web fase 1.25 - Formulario de Reservas

--- src/actions/booking.ts
+++ src/actions/booking.ts
@@ -5,15 +5,40 @@ import { z } from "astro:schema";
 export const booking = defineAction({
     accept: "form",
     input: z.object({
-        dateRange: z.string().min(1),
-    //     to: z.string(),
-    //     children: z.string().optional(),
-    //     rooms: z.string().optional(),
-    //     breakfast: z.string().optional(),
-    //     vehicle: z.string().optional(),
+        dateRange: z.string(),
+        adults: z.string(),
+        children: z.string(),
+        rooms: z.string(),
+        breakfast: z.string(),
+        vehicle: z.string(),
     }),
     handler: async (input) => {
-        console.log("Booking data:", input);
-        return { success: true };
-    },
-});
+        const {dateRange, ...rest} = input; // extrae dateRange y guarda el resto de propiedades
+
+        // Expresión regular para capturar las partes
+        const regex = /(\d{4}\/\d{2}\/\d{2})\s*➜\s*(\d{4}\/\d{2}\/\d{2})\s*\(([^)]+)\)/;
+
+        // Ejecutamos el match
+        const match = dateRange?.match(regex);
+
+        let checkin = null;
+        let checkout = null;
+        let nights = null;
+
+        if (match) {
+            [, checkin, checkout, nights] = match;
+        } else {
+            console.warn("No se encontraron coincidencias en dateRange:", dateRange);
+        }
+
+        return {
+            success: true,
+            processing: {
+                checkin,
+                checkout,
+                nights,
+                ...rest,
+            },
+        };
+    }
+})

161e47d5f91a9447467dd5dbcb1d74716e9851ba
WalterCun <waltercunbustamante@gmail.com>
2025-11-09 12:13:09 -0500

    Centralizar Interfaz de recuros y corregir funcionamiento de carrusel de imagenes

---
+++ src/types/resource.ts
@@ -0,0 +1,10 @@
+import type { ImageMetadata } from "astro";
+
+export type ResourceType = "image" | "video" | "youtube" | "auto";
+
+export interface Resource {
+	src: string | ImageMetadata;
+	type?: ResourceType;
+	alt: string;
+	poster?: string;
+}

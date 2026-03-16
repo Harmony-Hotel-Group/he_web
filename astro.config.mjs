38c0b4903559e8fe2d1d22ad1d392455a95bdb44
WalterCun <waltercunbustamante@gmail.com>
2026-03-15 21:52:12 -0500

    feat: add Preact integration and update dependencies in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import preact from "@astrojs/preact";
 
 const isDevEnvironment = process.env.NODE_ENV === "development";
 
@@ -25,6 +26,7 @@ export default defineConfig({
 		},
 	},
 	integrations: [
+		preact(),
 		tailwindcss(),
 		icon({ iconSets: [{ name: "astro", svg: { dir: "src/icons" } }] }),
 	],
